2b2bd881efe0340aa6a3bd429c73d0fec4869fff
Kamaliprakash <kamaliprakash0307@gmail.com>
2023-11-27 23:31:06 +0530

    DOM day 2

---
+++ script.js
@@ -0,0 +1,54 @@
+// const button = document.createElement("button")
+// button.setAttribute("type","button")
+// button.setAttribute("id","btn")
+// button.classList.add("btn","btn-danger")
+// button.innerText="click here to Wish"
+// button.addEventListener("click",()=>{
+//     document.body.style.backgroundColor="red"
+//     alert("Good eveing friends ")
+// })
+    
+// >>>>>>>>>>>document.body.append(button)
+
+// const div = document.getElementById("div1")
+// div.addEventListener("click",()=>div.style.backgroundColor="red")
+// div.addEventListener("dblclick",()=>div.style.backgroundColor="pink")
+
+// function foo(){
+//     const res = document.getElementById("email").value
+//     console.log(res);
+// }
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+// setTimeout vs setInterval
+//  let time=10000
+
+// setTimeout(()=>{
+//     document.getElementById("message").innerText="Displayed after 10 sec"
+//  },time)
+
+//   let x= setInterval(()=>{
+//     if(time == 0){
+//         clearInterval(x)
+//     }else{
+//         time -=1000
+//     document.getElementById("timer").innerText=+time/1000
+//   }
+// },1000)
+
+
+//!Use prompt to read a value from user and display it in the span element.
+// *prompt
+// const button = document.createElement("button");
+// const span = document.createElement("span");
+// button.innerText = "Click here";
+// button.className = "btn";
+// document.body.append(button, span);
+// button.addEventListener("click", foo);
+// function foo() {
+//   const element = prompt("Enter your value");
+//   span.innerText = element;
+// }
+
+   
+  
